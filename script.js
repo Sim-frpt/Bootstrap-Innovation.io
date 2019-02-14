8929b5d9c9de801998b7518d39c9f5841d447e17
Simon <simon.fraipont@gmail.com>
2019-02-14 17:01:10 +0100

    milieu exercice bootstrap, pas tiptop...

--- script.js
+++ script.js
@@ -3,11 +3,11 @@
 import "babel-polyfill"
 
 // import bootstrap's javascript part
-import 'bootstrap';
-
-import images from "/_shared/img/*.jpg"
-/*
-  Put the JavaScript code you want below.
-*/
-
-console.log("Hey look in your browser console. It works!");
+import 'bootstrap'
+import team from "/_shared/img/Team/*.jpeg"
+import logos from "/_shared/img/*.png"
+import reflexion from "/_shared/img/Reflexion.jpg"
+import joy from "/_shared/img/Joy.jpg"
+import climber from "/_shared/img/Climber.jpg"
+import jamal from "/_shared/img/Jamal.png"
+TeamImage.jpeg
